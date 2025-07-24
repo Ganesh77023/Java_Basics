bcd27929f063bc4baa516883740827b42b779501
Ganesh77023 <kadariganesh94@gmail.com>
2025-07-24 22:01:10 +0530

    Problem Solving

---
+++ PerfectNum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class PerfectNum{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("Enter Number : ");
+		int num=sc.nextInt();
+		int sum=0;
+
+		for(int i=1;i<num;i++){
+			if(num%i==0){
+				sum+=i;
+			}
+		}
+		if(num==sum){
+			System.out.println(num+" is a perfect Number");
+		}else{
+			System.out.println(num+" is not a perfect Number");
+		}
+	}
+}
---
+++ PrintNum.java
@@ -0,0 +1,9 @@
+class PrintNum{
+	public static void main(String args[]){
+		int num=100;
+		
+		for(int i=0;i<num;i++){
+			System.out.print(i+" ");
+		}
+	}
+}
